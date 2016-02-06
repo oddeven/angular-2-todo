fa3efef9cd722109f7ce8a11bd0bdfe1873a363b
Angular 2 Workshop <workshop-guest@oddeven.ch>
2016-02-06 20:23:42 +0100

    Added systemjs text and sass loader plugin and separated template and styles from component

--- src/todo-app/todo-app.ts
+++ src/todo-app/todo-app.ts
@@ -1,9 +1,13 @@
 import {Component} from 'angular2/core';
 
+import template from './todo-app.html!text';
+import './todo-app.scss!scss';
+
 @Component({
   selector: 'todo-app',
-  template: `
-    <h1>TODO's</h1>
-  `
+  template,
+  host: {
+    class: 'todo-app'
+  }
 })
 export class TodoApp {}
